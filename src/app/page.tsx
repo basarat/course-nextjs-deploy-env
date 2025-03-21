740b03cd230184310fcfe2ed52f3ea58fa875b04
basarat <basaratali@gmail.com>
2025-03-21 12:10:23 +1100

    better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Page() {
-  return <>STAGE: {process.env.STAGE}</>;
+  const STAGE = process.env.STAGE;
+
+  switch (STAGE) {
+    case "production":
+      return "You are running a production version";
+    case "preview":
+      return "You are looking at a preview of the website";
+    case "developement":
+      return "Development mode enabled";
+    default:
+      return "Are you sure you provided a valid STAGE?";
+  }
 }
